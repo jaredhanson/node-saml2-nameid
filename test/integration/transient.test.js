9bcc4b26ed1a6f88208e2f5b7f75ed8f2bf13d4e
Jared Hanson <jaredhanson@gmail.com>
2013-07-29 10:21:58 -0700

    Document transient format.

--- test/integration/transient.test.js
+++ test/integration/transient.test.js
@@ -34,4 +34,39 @@ describe('name identifier with transient format', function() {
     });
   });
   
+  describe('containing a name qualifier and SP name qualifier', function() {
+    var xml = '<saml:NameID NameQualifier="https://openidp.feide.no" SPNameQualifier="http://lucy.localtunnel.me/" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_00000000aaa0a0a00a000aa0a00aa00aa000a000a0</saml:NameID>';
+    
+    it('should parse correctly', function() {
+      var id = nameid.parse(xml);
+    
+      expect(id).to.be.an('object');
+      expect(Object.keys(id)).to.have.length(6);
+      expect(id.format).to.equal('transient');
+      expect(id.formatURI).to.equal('urn:oasis:names:tc:SAML:2.0:nameid-format:transient');
+      expect(id.value).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+      expect(id.id).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+      expect(id.qualifier).to.equal('https://openidp.feide.no');
+      expect(id.spQualifier).to.equal('http://lucy.localtunnel.me/');
+    });
+  });
+  
+  describe('containing a name qualifier, SP name qualifier, and SP provided ID', function() {
+    var xml = '<saml:NameID NameQualifier="https://openidp.feide.no" SPNameQualifier="http://lucy.localtunnel.me/" SPProvidedID="123456789" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_00000000aaa0a0a00a000aa0a00aa00aa000a000a0</saml:NameID>';
+    
+    it('should parse correctly', function() {
+      var id = nameid.parse(xml);
+    
+      expect(id).to.be.an('object');
+      expect(Object.keys(id)).to.have.length(7);
+      expect(id.format).to.equal('transient');
+      expect(id.formatURI).to.equal('urn:oasis:names:tc:SAML:2.0:nameid-format:transient');
+      expect(id.value).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+      expect(id.id).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+      expect(id.qualifier).to.equal('https://openidp.feide.no');
+      expect(id.spQualifier).to.equal('http://lucy.localtunnel.me/');
+      expect(id.spProvidedID).to.equal('123456789');
+    });
+  });
+  
 });
