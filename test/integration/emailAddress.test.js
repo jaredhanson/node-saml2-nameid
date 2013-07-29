8bea2276600d3b111ff17a1adab4b1432d25b5e3
Jared Hanson <jaredhanson@gmail.com>
2013-07-29 11:15:14 -0700

    Add emailAddress format parser.

---
+++ test/integration/emailAddress.test.js
@@ -0,0 +1,20 @@
+var nameid = require('index');
+
+describe('name identifier in transient format', function() {
+  
+  describe('with value', function() {
+    var xml = '<saml:NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress">user@mail.example.org</saml:NameID>';
+    
+    it('should parse correctly', function() {
+      var id = nameid.parse(xml);
+    
+      expect(id).to.be.an('object');
+      expect(Object.keys(id)).to.have.length(4);
+      expect(id.format).to.equal('emailAddress');
+      expect(id.formatURI).to.equal('urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress');
+      expect(id.value).to.equal('user@mail.example.org');
+      expect(id.email).to.equal('user@mail.example.org');
+    });
+  });
+  
+});
