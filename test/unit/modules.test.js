a72fedda6c8d32279d11747a7851eb2b707f829a
Jared Hanson <jaredhanson@gmail.com>
2013-07-29 08:44:54 -0700

    Initial commit.

---
+++ test/unit/modules.test.js
@@ -0,0 +1,13 @@
+var nameid = require('index');
+
+describe('saml2-nameid', function() {
+  
+  it('should export parse function', function() {
+    expect(nameid.parse).to.be.a('function');
+  });
+  
+  it('should export Parser constructor', function() {
+    expect(nameid.Parser).to.be.a('function');
+  });
+  
+});
