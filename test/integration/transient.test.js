6ef6cbe1bd240b7d976541d2f092942af8c5f6eb
Jared Hanson <jaredhanson@gmail.com>
2013-07-29 10:33:33 -0700

    Add unspecified format parser.

--- test/integration/transient.test.js
+++ test/integration/transient.test.js
@@ -1,6 +1,21 @@
 var nameid = require('index');
 
-describe('name identifier with transient format', function() {
+describe('name identifier in transient format', function() {
+  
+  describe('without optional attributes', function() {
+    var xml = '<saml:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_00000000aaa0a0a00a000aa0a00aa00aa000a000a0</saml:NameID>';
+    
+    it('should parse correctly', function() {
+      var id = nameid.parse(xml);
+    
+      expect(id).to.be.an('object');
+      expect(Object.keys(id)).to.have.length(4);
+      expect(id.format).to.equal('transient');
+      expect(id.formatURI).to.equal('urn:oasis:names:tc:SAML:2.0:nameid-format:transient');
+      expect(id.value).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+      expect(id.id).to.equal('_00000000aaa0a0a00a000aa0a00aa00aa000a000a0');
+    });
+  });
   
   describe('containing a name qualifier', function() {
     var xml = '<saml:NameID NameQualifier="https://openidp.feide.no" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_00000000aaa0a0a00a000aa0a00aa00aa000a000a0</saml:NameID>';
